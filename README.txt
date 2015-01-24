95f22f82863a4ba9514df5d5d0b6cc147da036a8
simitpatel <simitp@gmail.com>
2015-01-24 13:30:01 -0600

    Project Docs
    Final files for Course Project

---
+++ README.txt
@@ -0,0 +1,8 @@
+README
+
+The files included are based on the "Human Activity Recognition Using Smartphones Data Set" compiled by the University of California Irvine's Machine Learning Repository (herein referred to as the UCI data). Specifically, two data sets are included:
+
+1. A subset of the UCI data consisting of just the mean and standard deviations
+2. Determining the mean of UCI data based on the activities the subject was conducting as well as the specific subject.
+
+Both datasets are included in the enclosed file, run_analysis.R. For instructions on interpreting the R file and the meaning of the variables included in the file, please see the enclosed codebook.  
--- run_analysis.R
+++ run_analysis.R
@@ -2,22 +2,96 @@
 
 setwd("C:/Users/sipatel/Documents/R/project3")
 
+#install packages and instantiate libraries
+
+install.packages("dplyr")
+install.packages("reshape2")
+library(dplyr)
+library(reshape2)
+
 #read files
 
 traindata <- read.table("./train/X_train.txt")
 
 testdata <- read.table("./test/X_test.txt")
 
-#bind files?
+ytrain <- read.table("./train/Y_train.txt")
 
-fulldata <- rbind(traindata, testdata)
+ytest <- read.table("./test/Y_test.txt")
 
-#load and then add column names to fulldata
+subtest <- read.table("./test/subject_test.txt")
+
+subtrain <- read.table("./train/subject_train.txt")
+
+#Uses descriptive activity names to name the activities in the data set (TASK #3)
+
+names(ytest) <- c("Activity")
+names(ytrain) <- c("Activity")
+
+
+ytrain1 <- sub("1","WALKING",ytrain[,1],ignore.case=TRUE)
+ytrain2 <- sub("2","WALKING_UPSTAIRS",ytrain1,ignore.case=TRUE)
+ytrain3 <- sub("3","WALKING_DOWNSTAIRS",ytrain2,ignore.case=TRUE)
+ytrain4 <- sub("4","SITTING",ytrain3,ignore.case=TRUE)
+ytrain5 <- sub("5","STANDING",ytrain4,ignore.case=TRUE)
+ytrain6 <- sub("6","LAYING",ytrain5,ignore.case=TRUE)
+
+ytrainfinal <- as.data.frame(ytrain6)
+
+ytest1 <- sub("1","WALKING",ytest[,1],ignore.case=TRUE)
+ytest2 <- sub("2","WALKING_UPSTAIRS",ytest1,ignore.case=TRUE)
+ytest3 <- sub("3","WALKING_DOWNSTAIRS",ytest2,ignore.case=TRUE)
+ytest4 <- sub("4","SITTING",ytest3,ignore.case=TRUE)
+ytest5 <- sub("5","STANDING",ytest4,ignore.case=TRUE)
+ytest6 <- sub("6","LAYING",ytest5,ignore.case=TRUE)
+
+ytestfinal <- as.data.frame(ytest6)
+
+#bind x and y files
+
+names(ytestfinal) <- "Activity"
+names(ytrainfinal) <- "Activity"
+alltrain <- cbind(traindata,subtrain, ytrainfinal)
+alltest <- cbind(testdata, subtest, ytestfinal)
+
+#bind test and train files (TASK #1)
+fulldata <- rbind(alltrain, alltest)
+
+#Appropriately labels the data set with descriptive variable names.  (TASK #4)
 
 allnames <- read.table("./features.txt")
+morenames <- c("Subject","Activity")
 
-colnames(fulldata) <- c(as.character(allnames[,2]))
+callnames <- as.character(allnames[,2])
+allnames1 <- sub("BodyAcc"," Body Acceleration ",callnames,ignore.case=TRUE)
+allnames2 <- sub("GravityAcc"," Gravity Acceleration ",allnames1,ignore.case=TRUE)
+allnames3 <- sub("BodyGyro"," Body Gyroscope ",allnames2,ignore.case=TRUE)
 
-#subset for only columns that are mean and standard deviations
+colnames(fulldata) <- c(allnames3,morenames)
 
-stdmeans <- fulldata[,grep("mean|std",colnames(fulldata),ignore.case=TRUE)]
+#Extract only the measurements on the mean and standard deviation for each measurement. (TASK #2)
+
+stdmean <- fulldata[,grep("mean|std|Activity|Subject",colnames(fulldata),ignore.case=TRUE)]
+
+#From the data set in step 4, creates a second, independent tidy data set 
+#with the average of each variable for each activity and each subject. (TASK #5)
+
+tidysubject <- group_by(stdmean,Subject) %>% summarise_each(funs(mean))
+
+#drop the Activity column from tidysubject and replace it with NA values to allow for binding with activity table
+
+tidysubject <- tidysubject[,-c(88)]
+tidysubject <- mutate(tidysubject,Activity="NA")
+
+tidyactivity <- group_by(stdmean,Activity) %>% summarise_each(funs(mean))
+
+#drop the Subject column from tidyactivity and replace it with NA values to allow for binding with activity table
+
+tidyactivity <- tidyactivity[,-c(88)]
+tidyactivity <- mutate(tidyactivity,Subject="NA")
+
+tidy <- rbind(tidysubject,tidyactivity)
+
+#put activity and subject columns next to each other for cleanliness
+
+tidy <- tidy[c(88,1:87)]
